e19c9f0f01745a7e7b9aa386dda24489ac02f8f4
Augusto <werlang10@gmail.com>
2022-05-18 16:07:09 -0300

    finalizando

--- movies/main.js
+++ movies/main.js
@@ -1,11 +1,12 @@
 let APIKey = 'api_key=2a9aaec413a0b1c711b1aa6b96d008ad';
-let APIUrl = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&' + APIKey;
-let APISearch = 'https://api.themoviedb.org/3/search/movie?api_key=2a9aaec413a0b1c711b1aa6b96d008ad&query=';
+let APIUrl = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&' + APIKey + '&language=pt-BR';
+let APISearch = 'https://api.themoviedb.org/3/search/movie?api_key=2a9aaec413a0b1c711b1aa6b96d008ad&language=pt-BR&query=';
 let IMGPath = 'https://image.tmdb.org/t/p/w500/';
 
 let search = document.getElementById("search");
 let form = document.getElementById("form");
 let wrapper = document.getElementById("wrapper");
+let vote = document.querySelector("span");
 
 //Get popular movies
 getMovies(APIUrl);
@@ -35,9 +36,13 @@ function displayMovies(data) {
 
         movieEl.innerHTML = `
             <img src="${IMGPath + poster_path}" alt="${title}">
-            <h1>${title}</h1>
-            <p>${vote_average}</p>
-            <p>${overview}</p>
+            <div class="title">
+                <h1>${title}</h1>
+                <span class="vote ${voteColor(vote_average)}">${vote_average}</span>
+            </div>
+            <div class="overview">
+                <p>${overview}</p>
+            </div>
         `;
 
         wrapper.appendChild(movieEl);
@@ -55,3 +60,31 @@ form.addEventListener("submit", (e) => {
         search.value = '';
     }
 })
+
+function voteColor(vote) {
+    if (vote >= 7.8) {
+        return 'green';
+    } else if (vote >= 5) {
+        return 'yellow';
+    } else {
+        return 'red';
+    }
+}
+
+//Sticky header
+window.onscroll = function () {
+    header_fixed();
+}
+
+let header = document.getElementById("header");
+let sticky = header.offsetTop;
+
+function header_fixed() {
+    if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+        console.log("add")
+    } else {
+        header.classList.remove("sticky");
+        console.log("remove")
+    }
+}
