82d3c23fcc1f42d7a3dc81153fce5e4da8cfa7a1
Augusto <diehlgraziela@gmail.com>
2022-05-23 14:25:54 -0300

    finished

--- pokemon/main.js
+++ pokemon/main.js
@@ -1,36 +1,34 @@
+//adds event listener to input when user hits enter
 document.getElementById('form').addEventListener('submit', getAPI);
-//let wrapper = document.getElementById('wrapper');
 
+//Convert first letter to uppercase
 function capitalize(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
+//convert search string to lowercase
 function lowerCase(string) {
     return string.toLowerCase();
 }
 
 function getAPI(event) {
     event.preventDefault();
+    //get value from input "search"
     const search = document.getElementById('search').value;
+    //add "search" variable to lowerCase function
     const name = lowerCase(search);
 
     fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
         .then(response => response.json())
         .then(data => {
-            let id = data.id;
-            let characteristic = `https://pokeapi.co/api/v2/characteristic/${id}`;
-            console.log(characteristic)
-
             document.getElementById('wrapper').innerHTML = `
-                <div>
-                    <img src="${data.sprites.front_default}">
-                    <h2>${capitalize(data.name)}</h2>
-                    <p>Type: ${data.types[0].type.name}</p>
-                    <p>Height: ${data.height / 10} cm</p>
-                    <p>Weight: ${data.weight / 10} kg</p>
-                </div>
-                `
+            <div>
+                <img class="image" src="${data.sprites.front_default}">
+                <p class="id">#${data.id}</p>
+                <h2 class="name">${capitalize(data.name)}</h2>
+                <p class="type">${capitalize(data.types[0].type.name)}</p>
+            </div>
+            `
         })
         .catch(error => console.log(error));
-
 }
